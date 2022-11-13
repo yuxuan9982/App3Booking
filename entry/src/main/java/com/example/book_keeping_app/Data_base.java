865e962bc7c79cd7e269798f3fdcedd2622ccfb6
yuxuan <2309941940@qq.com>
2022-11-14 00:43:02 +0800

    11/13 ver3

--- entry/src/main/java/com/example/book_keeping_app/Data_base.java
+++ entry/src/main/java/com/example/book_keeping_app/Data_base.java
@@ -2,7 +2,8 @@ package com.example.book_keeping_app;
 
 import com.example.book_keeping_app.model.data_item;
 import ohos.data.orm.OrmDatabase;
+import ohos.data.orm.annotation.Database;
 
 @Database(entities = {data_item.class}, version = 1)
-public abstract class BookStore extends OrmDatabase {
+public abstract class Data_base extends OrmDatabase {
 }
